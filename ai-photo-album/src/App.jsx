47567723d7585f3190fd9095fab4bcdf391fa483
kapangyarihan <marjorydelrosario@gmail.com>
2024-10-30 23:03:50 +0800

    added forgot password form

--- ai-photo-album/src/App.jsx
+++ ai-photo-album/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import ForgotPassword from "./components/ForgotPassword";
 
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
